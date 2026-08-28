af06edcf16ef880a4d1a7abf4a26082163701bae
James Dicker <ajcardsstore@gmail.com>
2026-08-28 14:57:58 +0100

    wow

--- app/battle-network/page.tsx
+++ app/battle-network/page.tsx
@@ -1,9 +1,6 @@
 import BattleNetworkClient from "./BattleNetworkClient";
-import { getBattleNetworkInitialData } from "@/lib/battle-network-data";
-
 export const dynamic = "force-dynamic";
 
-export default async function BattleNetworkPage() {
-  const initialData = await getBattleNetworkInitialData();
-  return <BattleNetworkClient initialData={initialData} initialAgencyId="" />;
+export default function BattleNetworkPage() {
+  return <BattleNetworkClient initialData={{ agencies: [], battles: [] }} initialAgencyId="" />;
 }
